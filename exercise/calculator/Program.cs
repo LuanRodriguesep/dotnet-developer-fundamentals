0e8a7d23a2b97c6339da577b4aad0fb26df8775d
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2025-12-05 11:18:36 -0300

    editando funcao subtrair

--- exercise/calculator/Program.cs
+++ exercise/calculator/Program.cs
@@ -6,13 +6,7 @@ namespace Calculator
     {
         static void Main(string [] args)
         {
-           // Sum();
-           Subtraction();
-        }
-
-        static void Sum()
-        {
-             Console.Clear();
+            Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
             float value1 = float.Parse(Console.ReadLine());
@@ -26,30 +20,7 @@ namespace Calculator
 
             Console.WriteLine("");
 
-            Console.WriteLine("O resultado da soma é: " + result); // concatenacao
-
-            Console.ReadKey();
-        }
-
-        static void Subtraction()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Primeiro valor: ");
-            float value1 = float.Parse(Console.ReadLine());
-
-             Console.WriteLine("Segundo valor: ");
-            float value2 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-
-            float result = value1 - value2;
-
-            Console.WriteLine("");
-
-            Console.WriteLine($"O resultado da subtração é: {result}"); // interpolacao
-
-            Console.ReadKey();
+            Console.WriteLine("O resultado é: " + result);
         }
     }
 }
